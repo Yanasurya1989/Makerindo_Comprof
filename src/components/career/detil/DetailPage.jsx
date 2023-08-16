4f64999944a31a1831df863300e76334a0e9c267
YanaWebProgrammer <teukita15@gmail.com>
2023-08-16 11:45:09 +0700

    update font detil news

--- src/components/career/detil/DetailPage.jsx
+++ src/components/career/detil/DetailPage.jsx
@@ -16,12 +16,12 @@ const DetailPage = () => {
         <>
           <div className='grid grid-cols-1 mx-auto flex justify-center flex-col lg:flex-row items-center'>
             <div>
-              <h1 className='text-left  font-bold text-lg mb-10'>{selectedItem.title}</h1>
+              <h1 className='text-left font-light font-bold text-lg mb-10'>{selectedItem.title}</h1>
               <img src={selectedItem.image} className=' lg:w-[16rem] w-full h-[50vh] flex justify-center mx-auto lg:h-[70vh]  object-cover'/>
             </div>
             <div>
               
-              <p className='text-left mt-10'>{selectedItem.description}</p>
+              <p className='text-left mt-10 font-light'>{selectedItem.description}</p>
             </div>
           </div>
         </>
